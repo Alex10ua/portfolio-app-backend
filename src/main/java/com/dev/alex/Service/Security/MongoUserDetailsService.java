41d3757561e7e7b32a8d9cf98f7ae9ca0fd3574d
Oleksandr <sasha.lazoryak@gmail.com>
2026-02-13 18:50:31 +0100

    feat: Implement user authentication and authorization using Spring Security, MongoDB, and user registration endpoints.

---
+++ src/main/java/com/dev/alex/Service/Security/MongoUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.dev.alex.Service.Security;
+
+import com.dev.alex.Repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MongoUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = usersRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+
+        return User.withUsername(user.getUsername())
+                .password(user.getPasswordHash())
+                .roles("USER")
+                .build();
+    }
+}
